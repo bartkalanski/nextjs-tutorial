862b364d104dfaa36361b520f803f51c642626a4
Bart Kalanski <bkalanski@gmail.com>
2021-03-01 16:13:51 +0000

    Add styling

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quod dolorem, sapiente dignissimos voluptate sunt et ea necessitatibus esse impedit earum dolores doloribus quo blanditiis deserunt aliquam repudiandae magni minima!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quod dolorem, sapiente dignis </p>
+        </div>
+    )
+}
+
+export default About
