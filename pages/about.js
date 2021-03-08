8d4ad12d6790c0eb182229739aff74cc30fc691a
Bart Kalanski <bkalanski@gmail.com>
2021-03-08 17:45:26 +0000

    Fetch data

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,27 @@
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quod dolorem, sapiente dignissimos voluptate sunt et ea necessitatibus esse impedit earum dolores doloribus quo blanditiis deserunt aliquam repudiandae magni minima!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quod dolorem, sapiente dignis </p>
-        </div>
-    )
-}
+import Head from "next/head";
 
-export default About
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quod
+          dolorem, sapiente dignissimos voluptate sunt et ea necessitatibus esse
+          impedit earum dolores doloribus quo blanditiis deserunt aliquam
+          repudiandae magni minima!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quod
+          dolorem, sapiente dignis{" "}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,29 @@
-const Ninjas = () => {
-    return (
-        <div>
-            <h1>All Ninjas</h1>
-        </div>
-    )
-}
+import styles from '../../styles/Ninjas.module.css'
+import Link from 'next/link'
 
-export default Ninjas
+export const getStaticProps = async () => {
+  //runs at build time
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return { props: { ninjas: data } };
+};
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+          <a className={styles.single}>
+            {" "}
+            <h3>{ninja.name}</h3>{" "}
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Ninjas;
